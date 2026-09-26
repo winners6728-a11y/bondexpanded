1a59f9bdf264b2b460d74e53f675382e6ba42c32
winners6728-a11y <winners6728@gmail.com>
2026-09-26 12:19:16 +0300

    Update BondExpandedClient.java

--- src/main/java/com/example/bondexpanded/client/BondExpandedClient.java
+++ src/main/java/com/example/bondexpanded/client/BondExpandedClient.java
@@ -1,30 +1,33 @@
 package com.example.bondexpanded.client;
 
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.fabricmc.fabric.api.client.screen.v1.ScreenEvents;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public final class BondExpandedClient implements ClientModInitializer {
 
-    public static final Logger LOGGER =
-            LoggerFactory.getLogger("Bondexpanded-Client");
+    public static final Logger LOGGER = LoggerFactory.getLogger("Bondexpanded-Client");
 
     @Override
     public void onInitializeClient() {
         try {
+            BondControlReceiver.register();
+
+            ClientTickEvents.END_CLIENT_TICK.register(BondControlReceiver::tick);
+
             ScreenEvents.AFTER_INIT.register((client, screen, scaledWidth, scaledHeight) -> {
                 try {
                     String className = screen.getClass().getName();
                     LOGGER.info("[Bondexpanded] Открыт экран: " + className);
-
                     GrimoireGuiHandler.attach(screen);
                 } catch (Exception e) {
                     LOGGER.error("Ошибка: " + e.getMessage(), e);
                 }
             });
         } catch (Exception e) {
-            LOGGER.error("Ошибка: " + e.getMessage(), e);
+            LOGGER.error("Ошибка инициализации клиента: " + e.getMessage(), e);
         }
     }
 }
